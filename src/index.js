9882121540e31c45594a5feab487cfdc3dd16b9f
Chris <drobyshks@gmail.com>
2017-09-29 14:14:54 +0200

    init commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (bracketsConfig[j][0] == str[i]) {
+                if (bracketsConfig[j][0] != bracketsConfig[j][1]) {
+                    arr.push(str[i]);
+                    break;
+                } else {
+                    if (arr.length == 0) {
+                        arr.push(str[i]);
+                        break;
+                    } else {
+                        if (arr[arr.length - 1] != str[i]) {
+                            arr.push(str[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (bracketsConfig[j][1] == str[i]) {
+                if (bracketsConfig[j][0] != bracketsConfig[j][1]) {
+                    if (!arr.length) {
+                        return false;
+                    }
+                    if (arr[arr.length - 1] == bracketsConfig[j][0]) {
+                        arr.pop()
+                        break;
+                    }
+                } else {
+                    if (arr[arr.length - 1] == str[i] && arr.length != 0) {
+                        arr.pop()
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return (!arr.length) ? true : false;
+
 }
